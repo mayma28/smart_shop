a87c52271c3060ddb1c1645ee80921270915fe45
mayma28 <121638937+mayma28@users.noreply.github.com>
2024-03-08 19:53:39 +0100

    first commit

--- lib/provider/cartItem.dart
+++ lib/provider/cartItem.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:smartshop/models/product_model.dart';
 
-class CartItem extends ChangeNotifier {
-  List<ProductModel> products = [];
+class Cart with ChangeNotifier {
+  List<ProductModel> _products = [];
+  double _price = 0.0;
 
-  addProduct(ProductModel productModel) {
-    products.add(productModel);
+  void addProduct(ProductModel productModel) {
+    _products.add(productModel);
+    _price += productModel.price as double;
     notifyListeners();
+  }
+
+  void remove(ProductModel productModel) {
+    _products.add(productModel);
+    _price -= productModel.price as double;
+    notifyListeners();
+  }
+
+  int get count {
+    return _products.length;
+  }
+
+  double get totalprice {
+    return _price;
+  }
+
+  List<ProductModel> get cartitem {
+    return _products;
   }
 }
